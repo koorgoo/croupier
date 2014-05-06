094f82d5696774aa34a7adc090b613e053fb2734
Dima Kurguzov <koorgoo@gmail.com>
2014-05-06 16:46:27 +0400

    Tuned LESS

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,34 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     requirejs: {
-      compile: {
+      dev: {
         options: {
           optimize: 'none',
           mainConfigFile: 'app/scripts/config.js',
           out: 'croupier/static/croupier/app.js',
         }
+      },
+      prod: {
+        options: {
+          optimize: 'uglify',
+          mainConfigFile: 'app/scripts/config.js',
+          out: 'croupier/static/croupier/app.js',
+        }
       }
     },
-    uglify: {
-      app: {
-        files: {
-          'croupier/static/croupier/app.js': ['croupier/static/croupier/app.js']
-        }
+    less: {
+      dev: {
+        options: {
+          paths: ['app/styles'],
+        },
+        files: { 'croupier/static/croupier/app.css': 'app/styles/main.less' }
+      },
+      prod: {
+        options: {
+          paths: ['app/styles'],
+          cleancss: true
+        },
+        files: { 'croupier/static/croupier/app.css': 'app/styles/main.less' }
       }
     },
     watch: {
@@ -23,18 +38,25 @@ module.exports = function(grunt) {
           'app/scripts/**/*.coffee',
           'app/scripts/**/*.html'
         ],
-        tasks: ['requirejs'],
-        options: {
-          interrupt: true,
-        }
+        tasks: ['requirejs:dev'],
+        options: { interrupt: true }
+      },
+      styles: {
+        files: [
+          'app/styles/**/*.css',
+          'app/styles/**/*.less'
+        ],
+        tasks: ['less:dev'],
+        options: { interrupt: true }
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-requirejs');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('default', ['requirejs']);
-  grunt.registerTask('build', ['requirejs', 'uglify']);
+  grunt.registerTask('default', ['requirejs:dev', 'less:dev']);
+  grunt.registerTask('build', ['requirejs:prod', 'less:prod']);
 };
