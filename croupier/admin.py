a531909e4d810257fc9306bd848c6866a8ef1535
Dima Kurguzov <koorgoo@gmail.com>
2014-05-12 09:44:45 +0400

    Admin card search

--- croupier/admin.py
+++ croupier/admin.py
@@ -3,5 +3,11 @@ from django.contrib import admin
 from .models import Deck, Card
 
 
+class CardAdmin(admin.ModelAdmin):
+    search_fields = ('front', 'back')
+    list_display = ('front', 'deck')
+    list_filter = ('deck__name',)
+
+
 admin.site.register(Deck)
-admin.site.register(Card)
+admin.site.register(Card, CardAdmin)
